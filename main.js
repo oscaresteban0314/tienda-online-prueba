ea9de396909030e4773c791b4e3a6f2f0dc3f538
oscaresteban0314 <oatenas78@gmail.com>
2023-01-19 04:16:18 -0500

    index desde js

--- main.js
+++ main.js
@@ -5,11 +5,10 @@ const nav_menu = document.querySelector('.mobile-menu');
 const nav_carrito = document.querySelector('.navbar-shopping-cart');
 const aside = document.querySelector('.product-detail');
 const cerrar_aside =document.querySelector('.cerrar_aside');
-const product_card=document.querySelector('.product-card');
 const product_detail =document.querySelector('.product-detail-info');
 const product_detail_close =document.querySelector('.product-detail-info-close');
+const cards_container = document.querySelector('.cards-container');
 nav_email.addEventListener('click', toogledeskpotmenu);
-product_card.addEventListener('click', toogleproduct_card);
 product_detail_close.addEventListener('click', toogleproduct_card);
 cerrar_aside.addEventListener('click', toogglecloseaside);
 mobile_menu.addEventListener('click', toogledeskpotmenumobile);
@@ -58,4 +57,37 @@ function toogleshoppingcart() {
         nav_menu.classList.add('inactive');
         product_detail.classList.add('inactive');
     }
+}
+const product_list=[];
+
+for (var i = 0; i < 14; i++) {
+    product_list.push({
+        name:'bike'+ i,
+        price:120 + i+5,
+        Image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    })
+}
+
+for (product of product_list) {
+    const product_card_index= document.createElement("div");
+    product_card_index.classList.add('product-card');
+    const img_product = document.createElement("img");//okey
+    img_product.setAttribute('src', product.Image);//okey
+    const product_info = document.createElement("div");//okey
+    product_info.classList.add('product-info');//okey
+    const div_info = document.createElement("div");//okey
+    const product_price = document.createElement("p");//OKEY    
+    product_price.innerText="$" +product.price+",00" ;//OKEY
+    const product_name = document.createElement("p");//OKEY
+    product_name.innerText=product.name;//OKEY
+    div_info.append(product_price,product_name);
+    const img_add_cart_figure = document.createElement("figure"); //okey
+    const img_add_cart = document.createElement("img");//okey
+    img_add_cart.setAttribute('src', './icons/bt_add_to_cart.svg');//okey
+
+    img_add_cart_figure.appendChild(img_add_cart);
+    product_info.append(div_info,img_add_cart_figure);
+    product_card_index.append(img_product, product_info);
+
+    cards_container.appendChild(product_card_index);
 }
